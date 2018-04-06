2e25f99d332b9ec41aa280796b81d2cbce48e8c1
manakoike <mkoike00@mylangara.bc.ca>
2018-04-05 22:01:44 -0700

    with check box function

--- www/js/createAccount.js
+++ www/js/createAccount.js
@@ -13,9 +13,12 @@ function onDeviceReady() {
 		initializeLogin = true;
 	}
 
-	$('#btnSignUp').click(function(){
+	
 
-		console.log("dracarys");
+	$('#btnSignUp').click(function(){
+		if(document.getElementById('checkBox').checked){
+		// added username var
+		var name = $('#username').val();
 		var email = $('#txtEmail').val();
 		var password = $('#pass1').val();
 		var password2 = $('#pass2').val();
@@ -23,7 +26,8 @@ function onDeviceReady() {
 			console.log("Passwords don't match");
 			return
 		}
-
+		// Is not logging username on console, only Firefox
+		console.log(name);
 		console.log(email);
 		console.log(password);
 
@@ -35,11 +39,40 @@ function onDeviceReady() {
 		  console.log("create account page" + errorMessage);
 		  // ...
 		}).then(function(){
-			// direct user to login page
-			window.location.replace("./login.html");
+
+			console.log("checking function")
+			console.log(firebase.auth().currentUser);
+			
+			var user = firebase.auth().currentUser;
+		    // code to get user uid from auth
+		    var userKey = user.uid;
+
+			const userData = {
+				name: name,
+				email: email,
+			} 
+
+	    	console.log(userData);
+	        // set userKey to Local Storage
+	        localStorage.setItem('userKey', userKey);
+
+	        // path to set the user key data to Firebase
+	        firebase.database().ref('users/' + userKey).update(userData).then(function(){
+
+	          // direct user when successfull loged in to index page
+	          window.location.replace("./index.html");
+
+	      });
+
+
+		   
+			// // direct user to login page
+			// window.location.replace("./login.html");
 		});
 
-		
+		} else {
+			alert("read terms and conditions")
+		}
 	});
 
 };
