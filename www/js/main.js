c8f33eb43c3c2fe9f74645dd8ddf1dcd8e4df3e5
Michele Camara <mcamara01@mylangara.bc.ca>
2018-04-12 23:35:01 -0700

    remove of firebase api key and adition license file

--- www/js/main.js
+++ www/js/main.js
@@ -49,12 +49,7 @@
       // Initialize Firebase
 
       var config = {
-        apiKey: "AIzaSyDyRm3-_rS5aGpattuN-cT3VfxhtYirTfk",
-        authDomain: "mypetbookapp-4d393.firebaseapp.com",
-        databaseURL: "https://mypetbookapp-4d393.firebaseio.com",
-        projectId: "mypetbookapp-4d393",
-        storageBucket: "mypetbookapp-4d393.appspot.com",
-        messagingSenderId: "353025423183"
+        // Add your own firebase initialize snippet code here
       };
       firebase.initializeApp(config);
 
